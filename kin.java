aa2d2d588ac013382ecf5877ced9c164eede70c8
pandi123 <pandikumar201295@gmail.com>
2016-08-07 23:33:01 -0700

    kin.java
    fgad

---
+++ kin.java
@@ -0,0 +1,20 @@
+public class test
+ {
+ public static void main(String[]args)
+ {
+ int a[]=new a[30];
+ int b[]=new b[30];
+ 
+ for(int i=0;i<a.length;i++)
+ {
+ for(int j=i+1;j<a.length;j++)
+ {
+ if(a[0]==a[j])
+ {
+ count++;
+ }
+ }
+ }return count;
+ }
+ }
+ 
